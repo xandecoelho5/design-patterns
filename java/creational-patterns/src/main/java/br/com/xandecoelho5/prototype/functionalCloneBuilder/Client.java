0458e6c432fef14851740bbeb64e8819594470b1
xandecoelho5 <xandecoelho5@hotmail.com>
2022-06-07 17:45:36 -0300

    Alterado ordem dos printlns

--- java/creational-patterns/src/main/java/br/com/xandecoelho5/prototype/functionalCloneBuilder/Client.java
+++ java/creational-patterns/src/main/java/br/com/xandecoelho5/prototype/functionalCloneBuilder/Client.java
@@ -7,15 +7,16 @@ public class Client {
 
 	public static void main(String[] args) throws CloneNotSupportedException {
 		User user = new User("ToClone", 25, new Address("ABC Street", 1000));
-		System.out.println(user);
 
 		User cloneUser = user.cloneBuilder().withName("Cloned++").now();
-		System.out.println(cloneUser);
 
 		User anotherUser = cloneUser.cloneBuilder()
 				.withAge(100)
 				.withAddress(new Address("A Street", 500))
 				.now();
+
+		System.out.println(user);
+		System.out.println(cloneUser);
 		System.out.println(anotherUser);
 	}
 }
