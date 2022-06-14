04f07a7713f1c60166749412beebec4efe9b9fb9
xandecoelho5 <xandecoelho5@hotmail.com>
2022-06-13 21:52:38 -0300

    Adicionado validação de acordo com mês

--- java/behavioral-patterns/src/main/java/br/com/xandecoelho5/template/sales/service/template/BlackFriday.java
+++ java/behavioral-patterns/src/main/java/br/com/xandecoelho5/template/sales/service/template/BlackFriday.java
@@ -2,19 +2,22 @@ package br.com.xandecoelho5.template.sales.service.template;
 
 import br.com.xandecoelho5.template.sales.model.Cart;
 
+import java.util.Calendar;
+
 public class BlackFriday extends BestOfferTemplate {
 
-	public BlackFriday(Cart cart) {
-		super(cart);
-	}
+    public BlackFriday(Cart cart) {
+        super(cart);
+    }
 
-	@Override
-	public boolean isAppliable() {
-		return true;  //FIXME we should use calendar to be dinamic
-	}
+    @Override
+    public boolean isAppliable() {
+        Calendar now = Calendar.getInstance();
+        return now.get(Calendar.MONTH) == Calendar.NOVEMBER;
+    }
 
-	@Override
-	protected void calibrateVariables() {
-		priceFactor = 0.75d;
-	}
+    @Override
+    protected void calibrateVariables() {
+        priceFactor = 0.75d;
+    }
 }
